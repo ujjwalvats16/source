c40bd30d0e8bb046ad29b13a1c9227463b61c47e
ronidas39 <49780975+ronidas39@users.noreply.github.com>
2019-12-08 17:37:34 +0100

    Add files via upload

---
+++ NEO4J_TUTORIAL_44_SOURCE_CODE.py
@@ -0,0 +1,15 @@
+from neo4j import GraphDatabase
+
+driver=GraphDatabase.driver(uri="bolt://localhost:7687",auth=("neo4j","Rambo@1234"))
+
+session=driver.session()
+
+q2=''' 
+   create (a:COMPANY{name:"google"})
+   '''
+   
+q1=''' 
+   drop constraint on (a:COMPANY) ASSERT a.name is unique
+   '''   
+   
+session.run(q2)
