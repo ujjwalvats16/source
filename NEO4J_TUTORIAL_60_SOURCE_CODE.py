8656019d714c5aacf9e26958783d37499db5e67f
ronidas39 <49780975+ronidas39@users.noreply.github.com>
2019-12-26 09:27:03 +0100

    Add files via upload

---
+++ NEO4J_TUTORIAL_60_SOURCE_CODE.py
@@ -0,0 +1,14 @@
+from neo4j import GraphDatabase
+driver=GraphDatabase.driver(uri="bolt://localhost:7687",auth=("neo4j","Rambo@1234"))
+session=driver.session() 
+q2='''
+MATCH (A)
+WHERE A.NAME ="MESSI"
+RETURN KEYS(A)
+'''
+result=session.run(q2)
+for i in result:
+    print(i)
+
+
+
