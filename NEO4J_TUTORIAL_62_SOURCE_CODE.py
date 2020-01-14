8b720d4c158aca782799610f05deaf782f40e478
ronidas39 <49780975+ronidas39@users.noreply.github.com>
2020-01-14 20:35:18 +0100

    Add files via upload

--- NEO4J_TUTORIAL_62_SOURCE_CODE.py
+++ NEO4J_TUTORIAL_62_SOURCE_CODE.py
@@ -1,4 +1,50 @@
 
-
-
+from neo4j import GraphDatabase
+driver=GraphDatabase.driver(uri=("bolt://localhost:7687"),auth=("neo4j","Rambo@1234"))
+session=driver.session()
+qs="""
+match (n:SHIRTS)
+with (n) limit 1
+delete (n)
+"""
+qsd="""
+match (n:SHIRTS)
+return count(n)
+"""
+qt="""
+match (n:TSHIRTS)
+with (n) limit 1
+delete (n)
+"""
+qtd="""
+match (n:TSHIRTS)
+return count(n)
+"""
+qj="""
+match (n:JEANS)
+with (n) limit 1
+delete (n)
+"""
+qjd="""
+match (n:JEANS)
+return count(n)
+"""
+print("enter your choice , enter 1 for shirts,2 for tshirts,3 for jeans")
+s=input("enter your choice")
+s=int(s)
+if(s==1):
+ session.run(qs)
+ result=session.run(qsd)
+ print(list(result))
+if(s==2):
+ session.run(qt)
+ result=session.run(qtd)
+ print(list(result))
+if(s==3):
+ session.run(qj)
+ result=session.run(qjd)
+ print(list(result))   
     
+    
+
+
