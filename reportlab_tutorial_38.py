72bf34e326587220d619f45a04af40454e3f999d
ronidas39 <49780975+ronidas39@users.noreply.github.com>
2020-01-18 07:53:53 +0100

    Add files via upload

--- reportlab_tutorial_38.py
+++ reportlab_tutorial_38.py
@@ -1,13 +1,16 @@
-#tutorial 38
+#tutorial 39
 #create multiple shapes
 
 #reportlab graphics sub package to create multiple shapes
 
-from reportlab.graphics import shapes
-from reportlab.lib import colors
-drawing_obj=shapes.Drawing(400,400)
-drawing_obj.add(shapes.Rect(20,20,300,300,strokeColor=colors.green,fillColor=colors.green))
-drawing_obj.add(shapes.Rect(20,20,200,200,strokeColor=colors.red))
-drawing_obj.add(shapes.Circle(120,100,60,fillColor=colors.blue))
-drawing_obj.save(formats=["pdf"],fnRoot="tutorial38")
+from reportlab.graphics.barcode import code39
+from reportlab.platypus import SimpleDocTemplate,Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
+pdf=SimpleDocTemplate("tutorial39.pdf")
+flow_obj=[]
+styles=getSampleStyleSheet()
+codetext="-paste-colgatemax white-100gm-5usd-"
+code=code39.Standard39(codetext,barHeight=50)
+flow_obj.append(code)
+pdf.build(flow_obj)
 
