a9fc8e86a38404b3df06695dec896171c4fbe402
ronidas39 <49780975+ronidas39@users.noreply.github.com>
2020-02-10 18:50:07 +0100

    Add files via upload

---
+++ RUST_TUTORIAL_13.rs
@@ -0,0 +1,14 @@
+fn main(){
+    //mutable variable i with 0
+    let mut i=0;
+//initiating the while loop with condition i>10
+    while i<10 {
+        //executing the print and i +1 ,once i becomes 11 while condition will become false .
+        println!("test");
+        i=i+1;
+       
+
+        }
+       
+
+}
